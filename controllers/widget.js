232f5695cd58ca233627b1aaa3bcfcfdaf7bd1b4
Rene Pot <rene@topener.nl>
2019-03-11 16:09:50 +0100

    add ability to send messages

--- controllers/widget.js
+++ controllers/widget.js
@@ -1,7 +1,16 @@
 var calcs = require(WPATH('calcs'));
+var moment = require('/alloy/moment');
 
 var sections = [];
 var messages = {};
+var listviewBottom = 60;
+
+if ($.args.__parentSymbol && $.args.__parentSymbol.apiName === 'Ti.UI.Window') {
+    var window = $.args.__parentSymbol;
+    window.addEventListener('postlayout', init);
+} else {
+    alert('Chat widget needs to be nested in a window');
+}
 
 // @TODO: backbone listeners
 // fetch reset
@@ -15,12 +24,16 @@ $.ChatMessages.on('change', onChangeMessage);
 $.ChatMessages.on('remove destroy', onRemoveMessage);
 
 function onAddMessage(model) {
+
     var sectionIndex = findOrCreateSection(model.attributes.dateTime);
     messages[model.attributes.day].push(model.attributes.timestamp);
     messages[model.attributes.day].sort();
 
     var messagesIndex = messages[model.attributes.day].indexOf(model.attributes.timestamp);
     $.listView.sections[sectionIndex].insertItemsAt(messagesIndex, [generateListItem(model)],{animated: true});
+
+    var lastSection = $.listView.sections[$.listView.sections.length - 1];
+    setTimeout(scrollToLastItem,250) ;
 }
 
 function onChangeMessage(model){
@@ -84,7 +97,62 @@ function createSection(dateTime) {
 
 }
 
-exports.addMessage = function(data, bulk) {
+function handleKeyboardChange(e){
+
+    var platformHeight = Ti.Platform.displayCaps.platformHeight;
+    $.chatMessageWrapper.animate({
+        height: platformHeight - e.keyboardFrame.y + listviewBottom - (platformHeight !== e.keyboardFrame.y ? window.safeAreaPadding.bottom : 0),
+        duration: e.animationDuration * 1000
+    });
+
+    $.listView.animate({
+        bottom: platformHeight - e.keyboardFrame.y + listviewBottom - (platformHeight !== e.keyboardFrame.y ? window.safeAreaPadding.bottom : 0),
+        duration: e.animationDuration * 1000
+    }, scrollToLastItem);
+}
+
+function scrollToLastItem() {
+    var lastSection = $.listView.sections[$.listView.sections.length - 1];
+    $.listView.scrollToItem($.listView.sections.length - 1, lastSection.items.length -1, {animated: false});
+}
+
+function sendMessage() {
+    var text = $.chatBox.value;
+    $.chatBox.value = '';
+
+    var message = {
+        me: true,
+        message: text,
+        dateTime: moment(new Date()).format(),
+        id: new Date().getTime()
+    };
+    
+    exports.addMessage(message);
+
+    $.trigger('addedMessage', {
+        message: message,
+        updateId: updateId
+    });
+
+}
+
+function updateId(oldId, newId) {
+    var model = $.ChatMessages.get(oldId);
+    model.set({id: newId});
+    console.log($.ChatMessages.get(newId).toJSON());
+}
+
+function init() {
+    window.removeEventListener('postlayout', init);
+    
+    listviewBottom = 60 + window.safeAreaPadding.bottom;
+    $.chatMessageWrapper.height = listviewBottom + 'dp';
+    $.listView.bottom = listviewBottom + 'dp';
+
+    if (OS_IOS) Ti.App.addEventListener('keyboardframechanged', handleKeyboardChange);
+}
+
+exports.addMessage = function(data) {
     data.day = calcs.toDate(data.dateTime);
     data.timestamp = calcs.toUnix(data.dateTime);
 
@@ -94,7 +162,7 @@ exports.addMessage = function(data, bulk) {
 
 exports.addMessages = function(messages) {
     _.each(messages, function(data) {
-        exports.addMessage(data, true);
+        exports.addMessage(data);
     });
 }
 
